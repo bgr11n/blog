d79ac8c9481993e9f1a63887ddc104fdb23f9d0a
Bohdan Hryshchenko <bgr11n@gmail.com>
2015-07-10 18:32:25 +0300

    Modified example app

--- app.rb
+++ app.rb
@@ -5,15 +5,7 @@ Dir[File.join(".", "controllers/*.rb")].each { |f| require f }
 class App < Pine::App
   get '/', to: 'home#index'
 
-  namespace 'asd' do
-    get '/aa', to: 'asd#asd'
-
-    namespace 'asd2' do
-      get '/aa2', to: 'asd#asd'
-    end
+  namespace 'user' do
+    get '/me', to: 'user#me'
   end
-end
-
-class AppTwo < App
-  get '/', to: 'home#index2'
 end
--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < Pine::Controller
   def index
-    params.inspect
+    status 201
+    response.headers = { 'Content-Type' => 'application/json' }
+    request.request_method
   end
 end
