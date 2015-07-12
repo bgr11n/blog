652535e57a7dbe06e492bf5e570c5e7c513e0bd5
bgr11n <bgr11n@gmail.com>
2015-07-12 22:45:21 +0300

    Added to match all route

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ Dir[File.join(".", "controllers/*.rb")].each { |f| require f }
 class App < Pine::App
   get '/hello/:name', to: 'hello#index'
   get '/', to: [200, {}, ['Meow!']]
+  get '*', to: [200, {}, ['Meow! asd']]
 
   # namespace 'user' do
   #   get '/user', to: 'user#me'
