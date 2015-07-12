7b0a1150485d9f287b3b4ea4b5c231687b76a76e
bgr11n <bgr11n@gmail.com>
2015-07-12 22:38:00 +0300

    Test new router

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ Dir[File.join(".", "controllers/*.rb")].each { |f| require f }
 
 class App < Pine::App
   get '/hello/:name', to: 'hello#index'
-  get '/', to: 'home#index'
+  get '/', to: [200, {}, ['Meow!']]
 
   # namespace 'user' do
   #   get '/user', to: 'user#me'
